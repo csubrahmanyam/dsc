c10915286a92906cc633e9fd3d16187fdd70ed3c
csubrahmanyam <csubrahmanyam2@gmail.com>
2024-03-28 14:03:23 +0530

    version 1.0.0 huh!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
-use std::fs::File;
-use std::io;
+use std::fs;
+use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
 use whoami;
 
-const VERSION: &str = "0.1.0";
+const VERSION: &str = "1.0.0";
 
 #[derive(Debug)]
 struct DesktopEntry {
@@ -69,7 +71,10 @@ version: {VERSION}
             }
 
             println!("Enter full path to app icon (optional):");
-            let icon_path = input();
+            let mut icon_path = input();
+            if icon_path == "\n".to_owned() {
+                icon_path = "utilities-terminal".to_owned();
+            }
 
             println!("Always open app in a terminal(y/n): ");
             let terminal: bool = {
@@ -86,7 +91,7 @@ version: {VERSION}
                 icon_path,
                 terminal,
             };
-            // create_shortcut(desktop_entry);
+            create_shortcut(desktop_entry);
             break;
         }
     }
@@ -106,7 +111,59 @@ fn input() -> String {
     user_input
 }
 
-// fn create_shortcut(desktop_entry: DesktopEntry) {
-//     let short_cut = File::open("main.rs").unwrap();
-//     // println!("{}", whoami::username().len());
-// }
+fn create_shortcut(desktop_entry: DesktopEntry) {
+    // short-cut template
+    let short_cut_template = format!(
+        r#"[Desktop Entry]
+Type=Application
+Terminal={}
+Exec={}
+Name={}
+Icon={}
+Comment=app"#,
+        desktop_entry.terminal,
+        desktop_entry.app_path,
+        desktop_entry.app_name,
+        desktop_entry.icon_path
+    );
+
+    // short-cut file path
+    let short_cut_path: &str = &format!(
+        "/home/{}/.local/share/applications/{}.desktop",
+        whoami::username(),
+        desktop_entry.app_name
+    );
+    let short_cut_path = Path::new(&short_cut_path);
+
+    //Open the file
+    let mut short_cut_file: fs::File = match fs::File::open(&short_cut_path) {
+        Ok(file) => {
+            println!(
+                "A short-cut with name '{}' already exists!!",
+                desktop_entry.app_name
+            );
+            println!("Try any other name for your short-cut!");
+            file
+        }
+        Err(error) => match fs::File::create(&short_cut_path) {
+            Ok(file) => file,
+
+            Err(why) => panic!("Error!\n\nUnable to create the short-cut!"),
+        },
+    };
+
+    //write to file
+    match short_cut_file.write_all(short_cut_template.as_bytes()) {
+        Err(why) => panic!("Error:\n  Unable to create the short-cut!"),
+        Ok(_) => println!("Successfully created short-cut!\n\nWait for a moment..."),
+    };
+
+    //add permissions to file as executable
+    let mut permissions = short_cut_file.metadata().unwrap().permissions();
+
+    permissions.set_mode(0o744);
+    println!("Yep!,\n");
+    println!("-> Go to Linux Mint Menu");
+    println!("-> Search for name '{}'", desktop_entry.app_name);
+    println!("-> Right click on the name and\n select add to desktop or panel!");
+}
